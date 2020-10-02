ad532886fec19ce4cc1d877f9db661b79e544616
Thomas McMahon <tmcmahon@nerdery.com>
2020-10-02 11:37:19 -0500

    Updates

---
+++ precache-manifest.4a08b760ed37ff40c572097388e757c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa973c8731cf8e83bc1a9242a34788a",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "74df2f1b42448b689c99",
+    "url": "/simple-todo-app/static/css/main.b9ba9424.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74df2f1b42448b689c99",
+    "url": "/simple-todo-app/static/js/main.7e201e35.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
